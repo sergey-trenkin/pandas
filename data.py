8289704fc3053cf74eb202fdaf1313c4e8e19ddd
sergey-trenkin <140949059+sergey-trenkin@users.noreply.github.com>
2023-08-18 17:13:28 +0300

    Add files via upload

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ print(df['Category'].value_counts())
 temp = df['Content Rating'].value_counts()
 print('Соотношение:', round(temp['Teen'] / temp['Everyone 10+'], 2))
 
+
 temp = df.groupby(by = 'Type')['Rating'].mean()
 print(temp['Paid'])
 
@@ -13,8 +14,10 @@ print(round(temp['Paid'] - temp['Free'], 2))
 
 print(df.groupby(by = 'Category')['Size'].agg(['min', 'max']))
 
+
 temp = df[df['Rating'] > 4.5]['Category'].value_counts()
 print(temp['FINANCE'])
+
 
 temp = df[(df['Category'] == 'GAME') & (df['Rating'] > 4.9)]['Type'].value_counts()
 print(temp['Free'] / temp['Paid'])
